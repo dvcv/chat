f9a5a2cbe60d0229dca1c91feeb8aeb035891792
David Chavez <dvcv1992@gmail.com>
2017-08-30 01:16:53 -0700

    Finished basic setup

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+
+    function config($locationProvider, $stateProvider) {
+       $locationProvider
+           .html5Mode({
+               enabled: true, //the hashbang URLs are disabled
+               requireBase: false //avoids a common $location error
+           });
+
+       $stateProvider
+           .state('home', {
+               url: '/',
+               controller: 'HomeCtrl as home',
+               templateUrl: '/templates/home.html'
+           });
+    }
+    angular
+        .module('chat', ['ui.router', 'firebase'])
+        .config(config);
+})();
