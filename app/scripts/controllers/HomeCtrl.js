5adfb693329e2f5d30938acbf547cd1ee6a2dd3a
David Chavez <dvcv1992@gmail.com>
2017-09-08 17:25:57 -0700

    Refactored Code

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,38 +1,10 @@
 (function() {
-    function HomeCtrl(Room, $uibModal, $log) {
+    function HomeCtrl(Room, Modal) {
       this.rooms = Room.all;
-      this.animationsEnabled = true;
-      this.items = ['item1', 'item2', 'item3'];
-      //Open a modal to add new room
-      this.open = function (size) {
-         var modalInstance = $uibModal.open({
-           animation: this.animationsEnabled,
-           ariaLabelledBy: 'modal-title',
-           ariaDescribedBy: 'modal-body',
-           templateUrl: '/templates/modal.html',
-           controller: 'ModalInstanceCtrl',
-           controllerAs: 'modal',
-           size: size,
-           resolve: {
-             items: function () {
-               return this.items;
-             }
-           }
-         });
-
-         modalInstance.result.then(function (selectedItem) {
-           this.selected = selectedItem;
-         }, function () {
-           $log.info('Modal dismissed at: ' + new Date());
-         });
-       };
-
-       this.toggleAnimation = function () {
-         this.animationsEnabled = !this.animationsEnabled;
-       };
+      this.open = Modal.open;
     }
 
     angular
         .module('chat')
-        .controller('HomeCtrl', ['Room', '$uibModal', '$log', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Modal', HomeCtrl]);
 })();
